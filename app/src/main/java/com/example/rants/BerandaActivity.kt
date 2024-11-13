32f8ab280c19a7e0af02ff610a37332a01193464
unknown <adjielovano@gmail.com>
2024-11-13 08:22:47 -0800

    rants

---
+++ app/src/main/java/com/example/rants/BerandaActivity.kt
@@ -0,0 +1,16 @@
+package com.example.rants
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BerandaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beranda)
+
+    }
+}
