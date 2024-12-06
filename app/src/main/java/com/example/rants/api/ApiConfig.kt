2ffed43ddfb2dbb78790c134f6560410ce4280b8
Muhammadyafiheldy <m.yafiheldy@gmail.com>
2024-12-06 10:06:50 +0700

    tes running project

--- app/src/main/java/com/example/rants/api/ApiConfig.kt
+++ app/src/main/java/com/example/rants/api/ApiConfig.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiConfig {
-    private const val BASE_URL = "http://192.168.1.12:8000/api/"
+    private const val BASE_URL = "http://12.10.11.22:8000/api/"
 
     fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
